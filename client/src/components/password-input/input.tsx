cc9bcd146485f7bb780e8aa2bb7fd699018e4bd7
igr-ok <lfsvrgd@gmail.com>
2023-07-13 18:57:38 +0100

    wip: PasswdInput..

--- client/src/components/password-input/input.tsx
+++ client/src/components/password-input/input.tsx
@@ -1,10 +1,21 @@
 import { Form, Input } from 'antd';
 import { NamePath } from 'antd/es/form/interface';
 
-export const PasswordInput = () => {
-    return (
-        <div>
+type Props = {
+    name: string;
+    placeholder: string;
+    dependencies?: NamePath[]
+}
 
-        </div>
+export const PasswordInput = ({name, placeholder, dependencies}: Props) => {
+    return (
+        <Form.Item name={name} dependencies={dependencies} hasFeedback rules={[{
+            required: true,
+            message: 'Required field'
+        }, ({getFieldValue}) => ({
+            validator
+        })]}>
+            <Input.Password placeholder={placeholder} size='large' />
+        </Form.Item>
     )
 }
