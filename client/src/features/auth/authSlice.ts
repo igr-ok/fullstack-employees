f7f9ccd05dc9dabe1bd3e8769844504de10e3ab0
igr-ok <lfsvrgd@gmail.com>
2023-07-14 11:02:43 +0100

    wip: add reducers

--- client/src/features/auth/authSlice.ts
+++ client/src/features/auth/authSlice.ts
@@ -35,3 +35,10 @@ const slice = createSlice({
         })
     }
 })
+
+export const {logout} = slice.actions;
+export default slice.reducer;
+
+export const selectIsAuthenticated = (state: RootState) => state.auth.isAuthenticated;
+
+export const selectUser = (state: RootState) => state.auth.user;
